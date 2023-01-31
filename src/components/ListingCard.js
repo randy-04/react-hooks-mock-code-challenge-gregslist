da4bd5cab128c8f6722998992a217c25b8277386
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-01-31 08:51:57 +0300

    1. List cards. 2. Toggle favourite button.

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from 'react-bootstrap/Card';
 
-function ListingCard() {
+function ListingCard({ card }) {
+  // desctructuring prop
+  const {image, description, location} = card;
+
+  const [fav, setFav] = useState(false);
+
+  const handleFav = () => {
+    setFav((star) => {
+      // console.log("Fav?", star);
+      return !star;
+
+    })
+  }
+ 
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={image} alt={"description"} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {fav ? (
+          <button onClick={handleFav} className="emoji-button favorite active">★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button onClick={handleFav} className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
+        <strong>{description}</strong>
+        <span> · {location}</span>
         <button className="emoji-button delete">🗑</button>
       </div>
+
+    {/* <Card className="bg-dark text-white">
+          <Card.Img className="image" src={image} alt={description} />
+          <Card.ImgOverlay>
+            <Card.Title><span className="price">$0</span></Card.Title>
+            <Card.Text>
+            <div className="details">
+              {true ? (
+                <button className="emoji-button favorite active">★</button>
+              ) : (
+                <button className="emoji-button favorite">☆</button>
+              )}
+              <strong>{description}</strong>
+              <span> · {location}</span>
+              
+            </div>
+            </Card.Text>
+            <Card.Text><button className="emoji-button delete">🗑</button></Card.Text>
+          </Card.ImgOverlay>
+        </Card> */}
     </li>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,25 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  // usestate to hold cards state
+  const [cards, setCards] = useState([]);
+
+  // useEffect to fetch the data
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then((r)=>r.json())
+    .then((data) => setCards(data))
+  }, [])
+
+  const listcards = cards.map((card) => {
+    return <ListingCard card={card} key={card.id}/>
+  })
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listcards}
       </ul>
     </main>
   );
